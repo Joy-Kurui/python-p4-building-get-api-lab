75ee6fe5de9db0ad80ed463de5089d0d1ac648bf
joy Kurui <kurui.chepngetich@gmail.com>
2024-02-03 17:45:28 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -20,19 +20,78 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = {
+            "id": bakery.id, 
+            "name": bakery.name,
+            "created_at": bakery.created_at,
+            "updated_at": bakery.updated_at,
+            
+        }
+        bakeries.append(bakery_dict)
+
+    response = make_response(
+        jsonify(bakeries), 
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    
+    baked_goods_data = []
+    for bg in baked_goods:
+        baked_goods_dict = {
+            "id": bg.id, 
+            "name": bg.name, 
+            "price": bg.price,
+            "created_at": bg.created_at
+            } 
+        baked_goods_data.append(baked_goods_dict)
+
+    # response_data = {'baked_goods': baked_goods_data}
+
+    response = make_response(
+        jsonify(baked_goods_data),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    baked_goods_dict = baked_goods.to_dict()
+
+    response = make_response(
+        jsonify(baked_goods_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+ 
